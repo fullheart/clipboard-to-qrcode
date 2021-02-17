a4fbd9e847ad80ee89dad5a7fdec9399767bd3e5
fullheart <fullheart@protonmail.com>
2021-02-17 11:36:12 +0100

    feat: add installer for all Dependencies for a Kubuntu 20.04 system

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script from here: https://bram.xyz/blog/2012/04/clipboard-%E2%86%92-qr-code-in-console/
+## install dependencies:
+sudo apt-get install build-essential autoconf automake autotools-dev libtool pkg-config libpng-dev xsel
+## build + install 'qrencode' command
+curl -s https://fukuchi.org/works/qrencode/qrencode-4.1.1.tar.gz | tar xvz -C .
+cd qrencode-4.1.1/
+./configure
+make
+sudo make install
+sudo ldconfig
+
